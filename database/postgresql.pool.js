905462589a8601083b6c1d2be571e1df96d6f93b
Jonathan Vivero <jonathanviro37@gmail.com>
2022-09-07 23:32:06 -0500

    prueba

--- database/postgresql.pool.js
+++ database/postgresql.pool.js
@@ -13,9 +13,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
     connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
