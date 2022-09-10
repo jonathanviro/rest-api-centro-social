7dc9a51896b717fc447e0577bb9a05e94c28b01d
Jonathan Vivero <jonathanviro37@gmail.com>
2022-09-10 11:41:09 -0500

    pool modificado

--- database/postgresql.pool.js
+++ database/postgresql.pool.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgresql://postgres:admin123@35.184.192.111:5432/BD_CENTRO_SOCIAL`;
 
 const pool = new Pool({
     connectionString,
